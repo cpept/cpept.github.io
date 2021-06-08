3e69b99cfba814444fb4f255694020f81bc45fac
zlc350123 <pillow_c@163.com>
2021-06-09 00:22:02 +0800

    add0609

--- js/common.js
+++ js/common.js
@@ -1,17 +1,22 @@
-// nav状态栏下拉
-window.addEventListener('load', function () {
-    // 1. 获取元素
-    var nav = document.querySelector('.nav');
-    var lis = nav.children; // 得到4个小li
-    // 2.循环注册事件
-    for (var i = 0; i < lis.length; i++) {
-        lis[i].onmouseover = function () {
-            this.children[1].style.display = 'block';
-        }
-        lis[i].onmouseout = function () {
-            this.children[1].style.display = 'none';
-        }
-    }
+$(function () {
+    // nav状态栏下拉
+    $(".nav>li").hover(function () {
+        $(this).children("ul").stop().slideDown();
+    }, function () {
+        $(this).children("ul").hide();
+    })
 
+    // 顶部状态切换
+    $.fn.dingbu = function () {
+        if ($(document).scrollTop() > 0) {
+            $("header").addClass("h-moving");
+        } else {
+            $("header").removeClass("h-moving");
 
+        }
+    };
+    $(window).dingbu();
+    $(window).scroll(function () {
+        $(window).dingbu();
+    });
 })
